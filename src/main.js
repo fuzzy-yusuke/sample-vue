0e469ecc6b4e0866a71612e14fe2f89145a6ef7f
fuzzy-yusuke <youth_k0228@outlook.com>
2020-10-28 23:27:17 +0900

    firebase setup

--- src/main.js
+++ src/main.js
@@ -3,8 +3,29 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4ou1ptqJOJnurGgHzJjtEFxzXTqqWT3A",
+    authDomain: "my-address-pj-634f3.firebaseapp.com",
+    databaseURL: "https://my-address-pj-634f3.firebaseio.com",
+    projectId: "my-address-pj-634f3",
+    storageBucket: "my-address-pj-634f3.appspot.com",
+    messagingSenderId: "494726969142",
+    appId: "1:494726969142:web:cfdef3696558330a7e2597",
+    measurementId: "G-54S6BYCYNT"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 
 new Vue({
   //routerオプションにrouter変数を渡している
